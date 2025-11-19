9b48e21930d5ab138186506fc401c5048e9cc7cb
fullwall <fullwall25@gmail.com>
2025-11-19 14:55:34 +0800

    Remove some methods from NPCMoveEvent

--- src/main/java/net/citizensnpcs/api/event/NPCMoveEvent.java
+++ src/main/java/net/citizensnpcs/api/event/NPCMoveEvent.java
@@ -1,21 +1,21 @@
 package net.citizensnpcs.api.event;
 
-import net.citizensnpcs.api.npc.NPC;
+import java.util.Objects;
+
 import org.bukkit.Location;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Objects;
+import net.citizensnpcs.api.npc.NPC;
 
 public class NPCMoveEvent extends Event implements Cancellable {
-    private static final HandlerList handlers = new HandlerList();
-
-    private final NPC npc;
-    private Location from;
-    private Location to;
     private boolean cancelled;
+
+    private Location from;
+    private final NPC npc;
+    private Location to;
 
     public NPCMoveEvent(NPC npc, Location from, Location to) {
         this.npc = npc;
@@ -23,16 +23,32 @@ public class NPCMoveEvent extends Event implements Cancellable {
         this.to = to;
     }
 
-    public NPC getNPC() {
-        return npc;
-    }
-
     public Location getFrom() {
         return from;
     }
 
+    @NotNull
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public NPC getNPC() {
+        return npc;
+    }
+
     public Location getTo() {
         return to;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean b) {
+        this.cancelled = b;
     }
 
     public void setFrom(Location from) {
@@ -47,53 +63,14 @@ public class NPCMoveEvent extends Event implements Cancellable {
         this.to = to;
     }
 
-    public boolean hasChangedPosition() {
-        return hasExplicitlyChangedPosition() || !from.getWorld().equals(to.getWorld());
-    }
-
-    public boolean hasExplicitlyChangedPosition() {
-        return from.getX() != to.getX() || from.getY() != to.getY() || from.getZ() != to.getZ();
-    }
-
-    public boolean hasChangedBlock() {
-        return hasExplicitlyChangedBlock() || !from.getWorld().equals(to.getWorld());
-    }
-
-    public boolean hasExplicitlyChangedBlock() {
-        return from.getBlockX() != to.getBlockX() || from.getBlockY() != to.getBlockY() || from.getBlockZ() != to.getBlockZ();
-    }
-
-    public boolean hasChangedRotation() {
-        return from.getPitch() != to.getPitch() || from.getYaw() != to.getYaw();
-    }
-
-    @NotNull
     @Override
-    public HandlerList getHandlers() {
-        return handlers;
+    public String toString() {
+        return "NPCMoveEvent{" + "npc=" + npc + ", from=" + from + ", to=" + to + ", cancelled=" + cancelled + '}';
     }
 
     public static HandlerList getHandlerList() {
         return handlers;
     }
 
-    @Override
-    public boolean isCancelled() {
-        return cancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean b) {
-        this.cancelled = b;
-    }
-
-    @Override
-    public String toString() {
-        return "NPCMoveEvent{" +
-                "npc=" + npc +
-                ", from=" + from +
-                ", to=" + to +
-                ", cancelled=" + cancelled +
-                '}';
-    }
+    private static final HandlerList handlers = new HandlerList();
 }
